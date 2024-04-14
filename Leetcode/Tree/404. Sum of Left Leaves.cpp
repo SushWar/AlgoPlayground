d15418df458d4c55c89f7a53309cd507328570b6
Sushant Tanwar <130230991+SushWar@users.noreply.github.com>
2024-04-14 06:14:04 +0000

    Daily leetcode

--- Leetcode/Tree/404. Sum of Left Leaves.cpp
+++ Leetcode/Tree/404. Sum of Left Leaves.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
+ 
+ struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
 class Solution {
 public:
     int findSumTree(TreeNode* home, int incoming){
@@ -25,12 +25,9 @@ public:
            if(home->left == NULL && home->right == NULL){
                 return home->val;
            }
-
-            res += findSumTree(home->left, 1) + findSumTree(home->right, -1);
         }
-        else{
-            res += findSumTree(home->left, 1) + findSumTree(home->right, -1);
-        }
+        res += findSumTree(home->left, 1) + findSumTree(home->right, -1);
+       
 
         
         return res;
@@ -46,7 +43,7 @@ public:
         cin.tie(0);
         cout.tie(0);
 
-        
+
         int res = 0;
         if(root->left != NULL) res += findSumTree(root->left ,1);
         if(root->right != NULL) res += findSumTree(root->right, -1);
